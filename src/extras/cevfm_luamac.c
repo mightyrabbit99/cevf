598de75953645a86405d6f7f3d7165cfd31c8d78
mightyrabbit99 <t.1999ken@gmail.com>
2026-01-31 22:27:49 +0800

    add procedure functionality to luamac

--- src/extras/cevfm_luamac.c
+++ src/extras/cevfm_luamac.c
@@ -1,5 +1,6 @@
 #include <cevf_mod.h>
 #include <dirent.h>
+#include <pthread.h>
 
 #include "cvector.h"
 #include "cvector_utils.h"
@@ -7,13 +8,15 @@
 #include "lualib.h"
 #include "map.h"
 
-#define lge(...) do {\
-  fprintf(stderr, "[debug] %s %d: ", __FUNCTION__, __LINE__);\
-  fprintf(stderr, __VA_ARGS__);\
-} while (0)
+#define lge(...)                                                \
+  do {                                                          \
+    fprintf(stderr, "[debug] %s %d: ", __FUNCTION__, __LINE__); \
+    fprintf(stderr, __VA_ARGS__);                               \
+  } while (0)
 
 #define CEVF_LUA_MOD_MAIN_FUNCTOIN_NAME "cevf_main"
 #define CEVF_LUA_MOD_EVARR_NAME "cevf_events"
+#define CEVF_LUA_MOD_PCDNO_NAME "cevf_procedure_no"
 
 static lua_State *L = NULL;
 
@@ -36,26 +39,185 @@ static inline int _foreach_filenode(const char *path, int (*f)(const char *, voi
   return ret;
 }
 
-static hashmap *m_evtyp_modnamelst = NULL;
+static int _enqueue_f(lua_State *L) {
+  int res = -1;
+  size_t len;
+  if (!(lua_isstring(L, -2) && lua_isnumber(L, -1))) {
+    lua_pushnumber(L, res);
+    return 1;
+  }
+  const uint8_t *str = lua_tolstring(L, -2, &len);
+  cevf_evtyp_t evtyp = lua_tonumber(L, -1);
+  res = cevf_copy_enqueue(str, len, evtyp);
+  lua_pushnumber(L, res);
+  return 1;
+}
 
-static void _delete_m_evtyp_modnamelst_it(void *key, size_t ksize, uintptr_t value, void *usr) {
+static inline int _exec_mainf_t1(lua_State *L, const char *modname, const uint8_t *data, size_t datalen) {
+  lua_getglobal(L, modname);
+  if (!lua_istable(L, -1)) {
+    lua_pop(L, 1);
+    return 0;
+  }
+  lua_getfield(L, -1, CEVF_LUA_MOD_MAIN_FUNCTOIN_NAME);
+  if (!lua_isfunction(L, -1)) {
+    lua_pop(L, 1);
+    return 0;
+  }
+  lua_pushlstring(L, data, datalen);
+  lua_pushcfunction(L, _enqueue_f);
+  lua_call(L, 2, 1);
+  if (!lua_isnumber(L, -1)) {
+    lua_pop(L, 1);
+    return 0;
+  }
+  int res = lua_tonumber(L, -1);
+  lua_pop(L, 1);
+  return res;
+}
+
+static inline char *_exec_mainf_t2(lua_State *L, const char *modname, const uint8_t *data, size_t datalen, size_t *outlen) {
+  lua_getglobal(L, modname);
+  if (!lua_istable(L, -1)) {
+    lua_pop(L, 1);
+    return 0;
+  }
+  lua_getfield(L, -1, CEVF_LUA_MOD_MAIN_FUNCTOIN_NAME);
+  if (!lua_isfunction(L, -1)) {
+    lua_pop(L, 1);
+    return 0;
+  }
+  lua_pushlstring(L, data, datalen);
+  lua_pushcfunction(L, _enqueue_f);
+  lua_call(L, 2, 1);
+  if (!lua_isstring(L, -1)) {
+    lua_pop(L, 1);
+    return NULL;
+  }
+  const char *res2 = lua_tolstring(L, -1, outlen);
+  if (res2 == NULL) {
+    lua_pop(L, 1);
+    return NULL;
+  }
+  char *res = (char *)malloc(*outlen);
+  if (res == NULL) {
+    lua_pop(L, 1);
+    return NULL;
+  }
+  memcpy(res, res2, *outlen);
+  lua_pop(L, 1);
+  return res;
+}
+
+
+static hashmap *m_evtyp_modnamelst = NULL;
+pthread_mutex_t m_evtyp_modnamelst_mutex;
+static hashmap *m_pcdno_modname = NULL;
+pthread_mutex_t m_pcdno_modname_mutex;
+
+static char *luamac_generic_ctx = "luamac_generic_ctx";
+static uint8_t *luamac_generic_procedure_t2(const uint8_t *input, size_t input_len, size_t *output_len, void *ctx) {
+  char *ctx2 = (char *)ctx;
+  cevf_pcdno_t pcdno = (cevf_pcdno_t)(ctx2 - luamac_generic_ctx);
+  char *modname;
+  pthread_mutex_lock(&m_pcdno_modname_mutex);
+  if (!hashmap_get(m_pcdno_modname, &pcdno, sizeof(pcdno), (uintptr_t *)&(modname))) {
+    pthread_mutex_unlock(&m_pcdno_modname_mutex);
+    return NULL;
+  }
+  pthread_mutex_unlock(&m_pcdno_modname_mutex);
+
+  return (uint8_t *)_exec_mainf_t2(L, modname, input, input_len, output_len);
+}
+
+static void _delete_m_typ_modnamelst_it(void *key, size_t ksize, uintptr_t value, void *usr) {
   cvector_vector_type(char *) modnamelst = (cvector_vector_type(char *))value;
   cvector_for_each(modnamelst, free);
   cvector_free(modnamelst);
 }
 
-static inline void delete_m_evtyp_modnamelst(hashmap *m_evtyp_modnamelst) {
-  if (m_evtyp_modnamelst == NULL) return;
-  hashmap_iterate(m_evtyp_modnamelst, _delete_m_evtyp_modnamelst_it, NULL);
-  hashmap_free(m_evtyp_modnamelst);
+static inline void delete_m_typ_modnamelst(hashmap *m_typ_modnamelst) {
+  if (m_typ_modnamelst == NULL) return;
+  hashmap_iterate(m_typ_modnamelst, _delete_m_typ_modnamelst_it, NULL);
+  hashmap_free(m_typ_modnamelst);
 }
 
-#define _add_evtyp_modname(hmap, evtyp, modnamelst, modname)                \
-  do {                                                                      \
-    hashmap_get(hmap, &(evtyp), sizeof(evtyp), (uintptr_t *)&(modnamelst)); \
-    cvector_push_back(modnamelst, modname);                                 \
-    hashmap_set(hmap, &(evtyp), sizeof(evtyp), (uintptr_t)(modnamelst));    \
+static void _delete_m_typ_modname_it(void *key, size_t ksize, uintptr_t value, void *usr) {
+  char *modname = (char *)value;
+  free(modname);
+}
+
+static inline void delete_m_typ_modname(hashmap *m_typ_modname) {
+  if (m_typ_modname == NULL) return;
+  hashmap_iterate(m_typ_modname, _delete_m_typ_modname_it, NULL);
+  hashmap_free(m_typ_modname);
+}
+
+#define _add_typ_modname(hmap, typ, modname)                       \
+  do {                                                             \
+    cvector_vector_type(char *) lst__ = NULL;                      \
+    hashmap_get(hmap, &(typ), sizeof(typ), (uintptr_t *)&(lst__)); \
+    cvector_push_back(lst__, modname);                             \
+    hashmap_set(hmap, &(typ), sizeof(typ), (uintptr_t)(lst__));    \
   } while (0)
+
+static inline int _load_lua_mod_register_ev(lua_State *L, const char *_modname) {
+  lua_getfield(L, -1, CEVF_LUA_MOD_EVARR_NAME);
+  cevf_evtyp_t evtyp = CEVF_RESERVED_EV_THEND;
+  int arrlen;
+  if (lua_isnil(L, -1)) {
+    lua_pop(L, 1);
+    return 0;
+  }
+  char *modname = strdup(_modname);
+  if (modname == NULL) return -1;
+  if (lua_istable(L, -1) && (arrlen = lua_objlen(L, -1)) > 0) {
+    uint8_t has_valid_val = 0;
+    for (int i = 0; i < arrlen; i++) {
+      lua_rawgeti(L, -1, i);
+      if (!lua_isnumber(L, -1)) {
+        lua_pop(L, 1);
+        continue;
+      }
+      has_valid_val = 1;
+      evtyp = lua_tonumber(L, -1);
+      _add_typ_modname(m_evtyp_modnamelst, evtyp, modname);
+      lua_pop(L, 1);
+    }
+    if (has_valid_val == 0) {
+      _add_typ_modname(m_evtyp_modnamelst, evtyp, modname);
+    }
+  } else {
+    if (lua_isnumber(L, -1)) {
+      evtyp = lua_tonumber(L, -1);
+    }
+    _add_typ_modname(m_evtyp_modnamelst, evtyp, modname);
+  }
+  lua_pop(L, 1);
+  return 0;
+}
+
+static inline int _load_lua_mod_register_pcd(lua_State *L, const char *_modname) {
+  lua_getfield(L, -1, CEVF_LUA_MOD_PCDNO_NAME);
+  if (!lua_isnumber(L, -1)) {
+    lua_pop(L, 1);
+    return 0;
+  }
+  char *modname = strdup(_modname);
+  char *modname2 = NULL;
+  if (modname == NULL) return -1;
+  cevf_pcdno_t pcdno = lua_tonumber(L, -1);
+  if (hashmap_get(m_pcdno_modname, &pcdno, sizeof(pcdno), (uintptr_t *)&(modname2))) {
+    free(modname2);
+  }
+  hashmap_set(m_pcdno_modname, &pcdno, sizeof(pcdno), (uintptr_t)modname);
+  cevf_add_procedure_t2((struct cevf_procedure_t2_s){
+    .pcdno = pcdno,
+    .func = luamac_generic_procedure_t2,
+    .ctx = luamac_generic_ctx + pcdno,
+  });
+  return 0;
+}
 
 static int load_lua_mod(const char *file, void *ctx) {
   char *luamod_path = (char *)ctx;
@@ -78,33 +240,9 @@ static int load_lua_mod(const char *file, void *ctx) {
   lua_getfield(L, -1, CEVF_LUA_MOD_MAIN_FUNCTOIN_NAME);
   if (!lua_isfunction(L, -1)) goto not_a_module;
   lua_pop(L, 1);
-  lua_getfield(L, -1, CEVF_LUA_MOD_EVARR_NAME);
-  cevf_evtyp_t evtyp = CEVF_RESERVED_EV_THEND;
-  int arrlen;
-  cvector_vector_type(char *) modnamelst = NULL;
-  if (lua_istable(L, -1) && (arrlen = lua_objlen(L, -1)) > 0) {
-    uint8_t has_valid_val = 0;
-    for (int i = 0; i < arrlen; i++) {
-      lua_rawgeti(L, -1, i);
-      if (!lua_isnumber(L, -1)) {
-        lua_pop(L, 1);
-        continue;
-      }
-      has_valid_val = 1;
-      evtyp = lua_tonumber(L, -1);
-      _add_evtyp_modname(m_evtyp_modnamelst, evtyp, modnamelst, strdup(modname));
-      lua_pop(L, 1);
-    }
-    if (has_valid_val == 0) {
-      _add_evtyp_modname(m_evtyp_modnamelst, evtyp, modnamelst, strdup(modname));
-    }
-  } else {
-    if (lua_isnumber(L, -1)) {
-      evtyp = lua_tonumber(L, -1);
-    }
-    _add_evtyp_modname(m_evtyp_modnamelst, evtyp, modnamelst, strdup(modname));
-  }
-  lua_pop(L, 1);
+
+  _load_lua_mod_register_ev(L, modname);
+  _load_lua_mod_register_pcd(L, modname);
 
   lua_setglobal(L, file2);
   free(file2);
@@ -115,41 +253,15 @@ not_a_module:
   return 0;
 }
 
-static int _enqueue_f(lua_State *L) {
-  int res = -1;
-  size_t len;
-  if (!(lua_isstring(L, -2) && lua_isnumber(L, -1))) {
-    lua_pushnumber(L, res);
-    return 1;
-  }
-  const uint8_t *str = lua_tolstring(L, -2, &len);
-  cevf_evtyp_t evtyp = lua_tonumber(L, -1);
-  res = cevf_copy_enqueue(str, len, evtyp);
-  lua_pushnumber(L, res);
-  return 1;
-}
-
 static inline int _luamac_generic_evhandle(const uint8_t *data, size_t datalen, cevf_evtyp_t evtyp) {
   cvector_vector_type(char *) modnamelst = NULL;
+  pthread_mutex_lock(&m_evtyp_modnamelst_mutex);
   hashmap_get(m_evtyp_modnamelst, &(evtyp), sizeof(evtyp), (uintptr_t *)&(modnamelst));
+  pthread_mutex_unlock(&m_evtyp_modnamelst_mutex);
 
   char **modname;
   cvector_for_each_in(modname, modnamelst) {
-    lua_getglobal(L, *modname);
-    if (!lua_istable(L, -1)) {
-      lua_pop(L, 1);
-      continue;
-    }
-    lua_getfield(L, -1, CEVF_LUA_MOD_MAIN_FUNCTOIN_NAME);
-    if (!lua_isfunction(L, -1)) {
-      lua_pop(L, 1);
-      continue;
-    }
-    lua_pushlstring(L, data, datalen);
-    lua_pushcfunction(L, _enqueue_f);
-    lua_call(L, 2, 1);
-    int res = lua_tonumber(L, 1);
-    lua_pop(L, 1);
+    int res = _exec_mainf_t1(L, *modname, data, datalen);
     if (res) return res;
   }
   return 0;
@@ -170,13 +282,19 @@ static int luamac_init(int argc, char *argv[]) {
   L = luaL_newstate();
   luaL_openlibs(L);
   m_evtyp_modnamelst = hashmap_create();
+  pthread_mutex_init(&m_evtyp_modnamelst_mutex, NULL);
+  m_pcdno_modname = hashmap_create();
+  pthread_mutex_init(&m_pcdno_modname_mutex, NULL);
   _foreach_filenode(env_str, load_lua_mod, env_str);
   return 0;
 }
 
 static void luamac_deinit(void) {
   lua_close(L);
-  delete_m_evtyp_modnamelst(m_evtyp_modnamelst);
+  delete_m_typ_modnamelst(m_evtyp_modnamelst);
+  pthread_mutex_destroy(&m_evtyp_modnamelst_mutex);
+  delete_m_typ_modname(m_pcdno_modname);
+  pthread_mutex_destroy(&m_pcdno_modname_mutex);
 }
 
 static inline void mod_luamac_init(void) {
